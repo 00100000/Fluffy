e3db6e85d59e2da62762e5116770f1c1ac6660ce
Mustan Pathetic <patheticmustan@gmail.com>
2020-12-26 00:41:32 -0500

    stupid fucking enmap

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,9 @@
 const { prefix } = require('../config');
 
 module.exports = async client => {
-
     await client.logger.log(`Logged in as ${client.user.tag} (${client.user.id}) in ${client.guilds.cache.size} server(s).`);
     
-    const cmdHelp = client.commands.get('help', 'help.name');
-    
     client.user.setStatus('online');
-    client.user.setActivity(`${prefix + cmdHelp}`, { type: 'PLAYING' })
+    client.user.setActivity(`${prefix}help`, { type: 'PLAYING' })
         .catch(err => client.logger.error(err));
 };
