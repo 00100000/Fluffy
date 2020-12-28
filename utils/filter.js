14d5890cbc8ab7fb19e2408b471e754cfb333059
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2020-12-27 19:43:40 -0500

    I am actually retarded

--- utils/filter.js
+++ utils/filter.js
@@ -1,12 +1,12 @@
 function filter(message) {
     if (message.guild.member(message.author).hasPermission('MANAGE_SERVER')) {
-        return false;
+        return true;
     }
     const filtered = ['fuck', 'shit', 'asshole', 'dick', 'fagg', 'nigg', 'porn', 'vagina', 'dsc.gg', 'discord.gg', 'https:'];
     if (filtered.some((s) => message.content.toLowerCase().includes(s))) {
-        return true
+        return false;
     }
-    return false;
+    return true;
 }
 
 module.exports = {
