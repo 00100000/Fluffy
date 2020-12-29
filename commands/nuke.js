50cdef7c39dad2046bf49fba68708e7f36fe1570
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2020-12-29 13:57:16 -0500

    default value for nuke (tiny commit sorry)

--- commands/nuke.js
+++ commands/nuke.js
@@ -3,14 +3,16 @@ const { embedColor } = require('../config');
 const { noBotPerms, noPerms } = require('../utils/errors');
 
 exports.run = async (client, message, args) => {
+    // permissions
     let perms = message.guild.me.permissions;
     if (!perms.has('ADMINISTRATOR')) return noBotPerms(message, 'ADMINISTRATOR');
     if (!message.member.permissions.has('ADMINISTRATOR')) return noPerms(message, 'ADMINISTRATOR');
-
+    // command requirements
     let logs = client.channels.cache.get('790446527281627176');
-    if (!args[0]) return message.channel.send('You didn\'t tell me how many messages to nuke!');
+    // user issues
+    if (!args[0]) args[0] = 25;
     if (args[0] > 100 || args[0] < 1) return message.channel.send('You can only nuke between 1 and 100 messages!');
-
+    // action
     const nukeEmbed = new MessageEmbed()
         .setTitle('Nuked!')
         .addField('Amount of Messages', args[0], false)
@@ -19,17 +21,18 @@ exports.run = async (client, message, args) => {
         .addField('Server', message.guild.name + `(${message.guild.id})`, false)
         .setColor(embedColor)
         .setTimestamp();
-    // nuke event
     message.delete().then(() => {
         message.channel.bulkDelete(args[0], true);
     }).then(() => {
         logs.send(nukeEmbed);
+    }).catch(() => {
+        message.channel.send('There was an error while processing your request!');
     });
 };
 
 exports.help = {
     name: 'nuke',
     aliases: ['n'],
-    description: 'Purges up to 100 messages from a channel.',
-    usage: 'nuke <amount>'
+    description: 'Purges up to 100 messages from a channel (Default 25).',
+    usage: 'nuke [amount]'
 };
