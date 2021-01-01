2c7509aa329155b43baa925ddc2756e343683bd2
Mustan Pathetic <patheticmustan@gmail.com>
2020-12-31 22:26:52 -0500

    persistent mutes

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,8 +1,17 @@
 const { MessageEmbed } = require('discord.js');
 const { embedColor } = require('../config');
+const { jsonReadFile } = require('../utils/file');
 
-module.exports = (client, member) => {
+module.exports = async (client, member) => {
     let logs = client.channels.cache.get('790446588392243200');
+
+    let muted = await jsonReadFile("muted.json");
+    if (Object.keys(muted).includes(member.guild.id)) {
+        if (Object.keys(muted[member.guild.id]).includes(member.id)) {
+            let muteRole = member.guild.roles.cache.find(r => r.name === 'Muted');
+            member.roles.add(muteRole);
+        }
+    }
 
     const addEmbed = new MessageEmbed()
         .setTitle('Add Event')
