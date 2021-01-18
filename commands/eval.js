e3edffc32c2035f91d8178858955e12ef562785a
Mustan Pathetic <patheticmustan@gmail.com>
2021-01-18 12:21:35 -0500

    eval length checking

--- commands/eval.js
+++ commands/eval.js
@@ -5,7 +5,13 @@ exports.run = async (client, message, args) => {
     if (message.author.id !== owner) return message.channel.send('Only the owner of this bot can use this command!');
 
     try {
-        message.channel.send(eval(args.join(' ')));
+        // todo: possible paginate eval output, or give warning
+        const output = eval(args.join(' '));
+        
+        if (output.length === 0) return;
+        if (output.length > 2000) return message.channel.send(`Output is too big! (${output.length})`);
+
+        message.channel.send(output);
     } catch (err) {
         message.channel.send(`\`ERROR\` \`\`\`xl\n${err}\n\`\`\``);
     }
