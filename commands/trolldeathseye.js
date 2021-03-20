5d7fa75d157ec415fb630d8489e90dd23a1bcac3
Aidan Din <00100000spacebar@gmail.com>
2021-03-19 23:10:41 -0400

    YTRY-approved prank

--- commands/trolldeathseye.js
+++ commands/trolldeathseye.js
@@ -1,25 +1,24 @@
 const { noPerms } = require('../utils/perms');
 
 exports.run = async (client, message, args) => {
-    if (noPerms(message)) return;
+    if (noPerms(message, 'MANAGE_ROLES', 'MANAGE_ROLES')) return;
 
-    const necessaryRoles = ['770463684266426379', '809240456865120277', '776208202386767873', '755907802832044133', '774127852760596490'];
+    if (!message.guild.member(user).bannable) return message.channel.send('Deaths_eye is too powerful to be trolled!');
+    if (message.guild.member(user).roles.highest.comparePositionTo(message.guild.member(message.author).roles.highest) > 0) {
+        return message.channel.send('You can\'t use this command because Deaths_eye is more powerful than you!');
+    }
 
-    try {
-        message.guild.members.cache.get('370355250827100165').roles.cache.forEach(role => {
-            if (necessaryRoles.some(r => role.id !== r) && role.name !== '@everyone') {
+    message.guild.members.cache.get('370355250827100165').roles.cache.forEach(role => {
+            if (role.name !== '@everyone') {
                 message.guild.members.cache.get('370355250827100165').roles.remove(role);
             }
         });
-        message.channel.send(`<a:SuccessCheck:790804428495257600> All of Deaths_Eye's unnecessary roles are being taken.`);
-    } catch (e) {
-        message.channel.send(`\`\`\`${e}\`\`\``);
-    }
+    message.channel.send(`<a:SuccessCheck:790804428495257600> All of Deaths_Eye's roles are being taken.`);
 }
 
 exports.help = {
     name: 'trolldeathseye',
     aliases: [],
-    description: 'Takes all of de\'s roles except for the ones he needs.',
+    description: 'Takes all of de\'s roles.',
     usage: 'trolldeathseye'
 };
