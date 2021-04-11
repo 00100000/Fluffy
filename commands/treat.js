22fb63fb037fc28364577e6ebd2a9da65495f670
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2021-04-11 19:15:48 -0400

    More Dog Commands + ideas

---
+++ commands/treat.js
@@ -0,0 +1,14 @@
+const { noPerms } = require('../utils/perms');
+
+exports.run = async (client, message, args) => {
+    if (noPerms(message, 'SEND_MESSAGES', 'SEND_MESSAGES')) return;
+
+    message.channel.send('https://cdn.discordapp.com/attachments/826269508834295809/826872572024062102/treat.gif');
+}
+
+exports.help = {
+    name: 'treat',
+    aliases: [],
+    description: 'Give our loyal moderation bot a treat!',
+    usage: 'treat'
+};
